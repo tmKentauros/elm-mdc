03c187ab550f7e56a4c2839b37709dde87483e0d
Alexander Foremny <aforemny@posteo.de>
2018-10-20 12:34:06 +0200

    Update Demo.Checkbox

--- demo/Demo/Checkbox.elm
+++ demo/Demo/Checkbox.elm
@@ -10,42 +10,33 @@ import Material.Button as Button
 import Material.Checkbox as Checkbox
 import Material.FormField as FormField
 import Material.Options as Options exposing (cs, css, styled, when)
+import Material.Typography as Typography
 import Platform.Cmd exposing (Cmd, none)
 
 
 type alias Model m =
     { mdc : Material.Model m
-    , checkboxes : Dict Material.Index Checkbox
+    , states : Dict Material.Index (Maybe Bool)
     }
 
 
 defaultModel : Model m
 defaultModel =
     { mdc = Material.defaultModel
-    , checkboxes =
-        Dict.singleton "checkbox-indeterminate-checkbox"
-            { defaultCheckbox | checked = Nothing }
-    }
-
-
-type alias Checkbox =
-    { checked : Maybe Bool
-    , disabled : Bool
-    }
-
-
-defaultCheckbox : Checkbox
-defaultCheckbox =
-    { checked = Just False
-    , disabled = False
+    , states =
+        Dict.fromList
+            [ ( "checkbox-hero-checkbox-checked", Just True )
+            , ( "checkbox-hero-checkbox-unchecked", Just False )
+            , ( "checkbox-demo-checkbox-unchecked", Just False )
+            , ( "checkbox-demo-checkbox-indeterminate", Nothing )
+            , ( "checkbox-demo-checkbox-checked", Just True )
+            ]
     }
 
 
 type Msg m
     = Mdc (Material.Msg m)
-    | ToggleIndeterminate Material.Index
-    | ToggleDisabled Material.Index
-    | ToggleChecked Material.Index
+    | CheckboxClicked Material.Index
 
 
 update : (Msg m -> m) -> Msg m -> Model m -> ( Model m, Cmd m )
@@ -54,151 +45,87 @@ update lift msg model =
         Mdc msg_ ->
             Material.update (lift << Mdc) msg_ model
 
-        ToggleIndeterminate index ->
+        CheckboxClicked index ->
             let
-                checkboxes =
-                    Dict.get index model.checkboxes
-                        |> Maybe.withDefault defaultCheckbox
-                        |> (\checkboxState ->
-                                Dict.insert index
-                                    { checkboxState
-                                        | checked =
-                                            if checkboxState.checked == Nothing then
-                                                Just False
-                                            else
-                                                Nothing
-                                    }
-                                    model.checkboxes
-                           )
-            in
-            ( { model | checkboxes = checkboxes }, Cmd.none )
+                state =
+                    Maybe.withDefault Nothing (Dict.get index model.states)
 
-        ToggleDisabled index ->
-            let
-                checkboxes =
-                    Dict.get index model.checkboxes
-                        |> Maybe.withDefault defaultCheckbox
-                        |> (\checkboxState ->
-                                Dict.insert index
-                                    { checkboxState | disabled = not checkboxState.disabled }
-                                    model.checkboxes
-                           )
-            in
-            ( { model | checkboxes = checkboxes }, Cmd.none )
+                newState =
+                    case state of
+                        Nothing ->
+                            Just True
 
-        ToggleChecked index ->
-            let
-                checkboxes =
-                    Dict.get index model.checkboxes
-                        |> Maybe.withDefault defaultCheckbox
-                        |> (\checkboxState ->
-                                Dict.insert index
-                                    { checkboxState
-                                        | checked =
-                                            if checkboxState.checked == Nothing then
-                                                Just True
-                                            else
-                                                Maybe.map not checkboxState.checked
-                                    }
-                                    model.checkboxes
-                           )
+                        Just False ->
+                            Just True
+
+                        Just True ->
+                            Just False
+
+                newStates =
+                    Dict.insert index newState model.states
             in
-            ( { model | checkboxes = checkboxes }, Cmd.none )
+            ( { model | states = newStates }, Cmd.none )
+
+
+checkbox : (Msg m -> m) -> String -> Model m -> Html m
+checkbox lift index model =
+    let
+        state =
+            Maybe.withDefault Nothing (Dict.get index model.states)
+    in
+    Checkbox.view (lift << Mdc)
+        index
+        model.mdc
+        [ cs "demo-checkbox"
+        , Options.onClick (lift (CheckboxClicked index))
+        , case state of
+            Nothing ->
+                Options.nop
+
+            Just checked ->
+                Checkbox.checked checked
+        ]
+        []
 
 
 view : (Msg m -> m) -> Page m -> Model m -> Html m
 view lift page model =
-    let
-        example options =
-            styled Html.div
-                (cs "example"
-                    :: css "display" "block"
-                    :: css "margin" "24px"
-                    :: css "padding" "24px"
-                    :: options
-                )
-    in
-    page.body "Checkbox"
-        [ let
-            viewCheckbox index =
-                let
-                    checkbox =
-                        Dict.get index model.checkboxes
-                            |> Maybe.withDefault defaultCheckbox
-                in
-                Checkbox.view (lift << Mdc)
-                    index
-                    model.mdc
-                    [ Options.on "click" (Json.succeed (lift (ToggleChecked index)))
-                    , when (checkbox.checked /= Nothing) <|
-                        Checkbox.checked (Maybe.withDefault False checkbox.checked)
-                    , Checkbox.disabled |> when checkbox.disabled
-                    ]
-                    []
-          in
-          Page.hero []
-            [ FormField.view []
-                [ viewCheckbox "checkbox-hero-checkbox"
-                , Html.label [ Html.for "checkbox-hero-checkbox" ] [ text "Checkbox" ]
+    page.demoPage
+        { title = "Checkbox"
+        , prelude =
+            [ """
+              Checkboxes allow the user to select multiple options from a set.
+              """
+            ]
+        , resources =
+            { materialGuidelines = ""
+            , documentation = ""
+            , sourceCode = ""
+            }
+        , hero =
+            [ Html.div []
+                [ checkbox lift "checkbox-hero-checkbox-checked" model
+                , checkbox lift "checkbox-hero-checkbox-unchecked" model
                 ]
             ]
-        , let
-            viewCheckbox index label =
-                let
-                    checkbox =
-                        Dict.get index model.checkboxes
-                            |> Maybe.withDefault defaultCheckbox
-                in
-                styled Html.div
-                    []
-                    [ FormField.view []
-                        [ Checkbox.view (lift << Mdc)
-                            index
-                            model.mdc
-                            [ Options.on "click" (Json.succeed (lift (ToggleChecked index)))
-                            , when (checkbox.checked /= Nothing) <|
-                                Checkbox.checked (Maybe.withDefault False checkbox.checked)
-                            , Checkbox.disabled |> when checkbox.disabled
-                            ]
-                            []
-                        , Html.label [ Html.for index ] [ text label ]
-                        ]
-                    , styled Html.div
-                        []
-                        [ Button.view (lift << Mdc)
-                            (index ++ "button-toggle-indeterminate")
-                            model.mdc
-                            [ Options.on "click" (Json.succeed (lift (ToggleIndeterminate index)))
-                            ]
-                            [ text "Toggle "
-                            , Html.code [] [ text "indeterminate" ]
-                            ]
-                        , Button.view (lift << Mdc)
-                            (index ++ "button-toggle-disabled")
-                            model.mdc
-                            [ Options.on "click" (Json.succeed (lift (ToggleDisabled index)))
-                            ]
-                            [ text "TOGGLE "
-                            , Html.code [] [ text "disabled" ]
-                            ]
-                        ]
-                    ]
-          in
-          example []
-            (List.concat
-                [ [ styled Html.h2
-                        [ css "margin-left" "0"
-                        , css "margin-top" "0"
-                        ]
-                        [ text "Checkbox" ]
-                  ]
-                , [ ( "checkbox-default-checkbox", "Default checkbox" )
-                  , ( "checkbox-indeterminate-checkbox", "Indeterminate checkbox" )
-                  ]
-                    |> List.map
-                        (\( index, label ) ->
-                            viewCheckbox index label
-                        )
-                ]
-            )
-        ]
+        , content =
+            [ styled Html.h3 [ Typography.subtitle1 ] [ text "Unchecked" ]
+            , checkbox lift "checkbox-demo-checkbox-unchecked" model
+            , styled Html.h3 [ Typography.subtitle1 ] [ text "Indeterminate" ]
+            , checkbox lift "checkbox-demo-checkbox-indeterminate" model
+            , styled Html.h3 [ Typography.subtitle1 ] [ text "Checked" ]
+            , checkbox lift "checkbox-demo-checkbox-checked" model
+            , Html.node "style"
+                [ Html.type_ "text/css" ]
+                [ text style ]
+            ]
+        }
+
+
+style : String
+style =
+    """
+    .demo-checkbox {
+      margin: 8px 16px;
+    }
+    """
